899e34fb9eb13fe3e567bb9a8e0d801697d6d1f3
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2024-03-10 00:56:17 +0100

    refactoring

--- kone.bouchra.py
+++ kone.bouchra.py
@@ -1,3 +1,4 @@
+#Author: Largaton KONE and Salma BOUCHRA
 import pulp
 from pulp import (
     PULP_CBC_CMD,
@@ -10,10 +11,21 @@ from pulp import (
     LpInteger,
 )
 
-def optimisation(demandes, cout_stockage, cout_approv):
-    prob = LpProblem(name='Cabines', sense=LpMinimize)
 
-    n = len(demandes)
+# ============================================================================ #
+#                                  SET MODEL                                   #
+# ============================================================================ #
+
+def optimisation(demandes, cout_stockage, cout_approv):
+    # ------------------------------------------------------------------------ #
+    # Linear problem with minimization
+    # ------------------------------------------------------------------------ #
+    prob = LpProblem(name='Cabines', sense=LpMinimize)
+    # ------------------------------------------------------------------------ #
+    # The variables
+    # ------------------------------------------------------------------------ #
+
+    n = len(demandes) 
 
     comm =[
             LpVariable(f'Commandes{i}', lowBound=0, cat=LpBinary)
@@ -29,10 +41,16 @@ def optimisation(demandes, cout_stockage, cout_approv):
             LpVariable(f'Approvisionnement{i}', lowBound=0, cat=LpInteger)
         for i in range(n)
         ]
+    
+    # ------------------------------------------------------------------------ #
+    # The objective function
+    # ------------------------------------------------------------------------ #
         
 
     prob += cout_approv * lpSum(comm)+ cout_stockage * lpSum(stock)
-    
+    # ------------------------------------------------------------------------ #
+    # The constraints
+    # ------------------------------------------------------------------------ #
 
     prob += approv[0] == lpSum ([stock[0] , demandes[0]])
     prob += approv[n-1]+ stock[n-2] == demandes[n-1]
@@ -46,43 +64,52 @@ def optimisation(demandes, cout_stockage, cout_approv):
         prob += approv[i] <= sum (demandes)*comm[i]
 
 
-    # Résolution du problème
+# ============================================================================ #
+#                               SOLVE WITH DATA                                #
+# ============================================================================ #
     prob.solve( PULP_CBC_CMD(
             msg=False, logPath='./CBC_adm_array.log',
         ),
     )
 
+
+# ============================================================================ #
+#                                   UTILITIES                                  #
+# ============================================================================ #
     print()
     print('-' * 40)
-    print("Statistiques")
+    print('Statistiques')
+    print('-' * 40)
     print()
-    print("-" * 40)
+    
+    print(f'Number variables: {prob.numVariables()}')
+    print(f'Number constraints: {prob.numConstraints()}')
     print()
-    print(f"Nombre de variables: {len(prob.variables())}")
-    print(f"Nombre de contraintes: {len(prob.constraints.values())}")
-    print()
+   
+
+  
     print('Time:')
     print(f'- (real) {prob.solutionTime}')
     print(f'- (CPU) {prob.solutionCpuTime}')
-    print()
+   
 
     print(f'Solve status: {LpStatus[prob.status]}')
     print(f'Objective value: {prob.objective.value()}')
 
-    print(f"Valeur de la stratégie optimale : {pulp.value(prob.objective)}")
-    print("Description de la stratégie optimale :")
-    print("-" * 83)
-    print("| Mois | Approvisionnement | Stock | Commandes | Demandes |")
-    print("-" * 83)
+    print(f'Valeur de la stratégie optimale : {pulp.value(prob.objective)}')
+    print('Description de la stratégie optimale :')
+    print('-' * 80)
+    print('| Mois | Approvisionnement | Stock | Commandes | Demandes |')
+    print('-' * 80)
     for i in range(n):
         mois = i
-        approvisionnement = approv[i].varValue 
+        approvisionnement = approv[i].varValue
         stocks = stock[i].varValue
         commandes = comm[i].varValue
         dmd = demandes[i]
-        print(f"| {mois:<4} | {approvisionnement:<17} | {stocks:<5} | {commandes:<8} | {dmd:<8} |")
+        print(f'| {mois:<4} | {approvisionnement:<17} | {stocks:<5} | {commandes:<8} | {dmd:<8} |')
 
-    print("-" * 83)
+    print('-' * 80)
 
 # Test
 demandes = [100, 200, 500, 700, 1000, 200, 300]
