e4e54c21f409010c81b7bee87470c4f2fae2c1e8
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2024-03-09 22:06:20 +0100

    amelioration contrainte

--- kone.bouchra.py
+++ kone.bouchra.py
@@ -1,10 +1,13 @@
 import pulp
 from pulp import (
+    PULP_CBC_CMD,
     LpMinimize,
     LpProblem,
     LpVariable,
     lpSum,
     LpStatus,
+    LpBinary,
+    LpInteger,
 )
 
 def optimisation(demandes, cout_stockage, cout_approv):
@@ -12,22 +15,42 @@ def optimisation(demandes, cout_stockage, cout_approv):
 
     n = len(demandes)
 
-
-    comm = LpVariable.dicts("Commandes", range(n), lowBound=0, cat='Continuous')
-    stock = LpVariable.dicts("Stocks", range(n + 1), lowBound=0, cat='Continuous')
-    approv = LpVariable.dicts("Approvisionnement", range(n), cat='Binary')
-
-    prob += lpSum(cout_stockage * stock[i] for i in range(1, n + 1)) + \
-            lpSum(cout_approv * approv[i] for i in range(n))
+    comm =[
+            LpVariable(f'Commandes{i}', lowBound=0, cat=LpBinary)
+        for i in range(n)
+        ]
     
-    prob += stock[0] == 0
+    stock =[
+            LpVariable(f'Stocks{i}', lowBound=0, cat=LpInteger)
+        for i in range(n)
+        ]
+    
+    approv =[
+            LpVariable(f'Approvisionnement{i}', lowBound=0, cat=LpInteger)
+        for i in range(n)
+        ]
+        
+
+    prob += cout_approv * lpSum(comm)+ cout_stockage * lpSum(stock)
+    
+
+    prob += approv[0] == lpSum ([stock[0] , demandes[0]])
+    prob += approv[n-1]+ stock[n-2] == demandes[n-1]
+
+    for i in range(1, n-1):
+
+        prob += approv[i] + stock[i-1]  == stock[i] + demandes[i]
+        
     for i in range(n):
-        prob += stock[i] + comm[i] - demandes[i] == stock[i + 1]
-        prob += comm[i] <= 10000 * approv[i]
-        prob += comm[i] >= 0.0001 * approv[i]
+        prob += comm[i] <= approv[i]
+        prob += approv[i] <= sum (demandes)*comm[i]
+
 
     # Résolution du problème
-    prob.solve()
+    prob.solve( PULP_CBC_CMD(
+            msg=False, logPath='./CBC_adm_array.log',
+        ),
+    )
 
     print()
     print('-' * 40)
@@ -51,11 +74,10 @@ def optimisation(demandes, cout_stockage, cout_approv):
     print("-" * 83)
     print("| Mois | Approvisionnement | Stock | Commandes | Demandes |")
     print("-" * 83)
-
     for i in range(n):
         mois = i
-        approvisionnement = comm[i].varValue if approv[i].varValue == 1.0 else 0.0
-        stocks = stock[i + 1].varValue
+        approvisionnement = approv[i].varValue 
+        stocks = stock[i].varValue
         commandes = comm[i].varValue
         dmd = demandes[i]
         print(f"| {mois:<4} | {approvisionnement:<17} | {stocks:<5} | {commandes:<8} | {dmd:<8} |")
